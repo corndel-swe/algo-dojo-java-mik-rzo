d5c2bfd9049d526f6dfbb2ce9a58e21364605755
mik-rzo <michael.panong3@gmail.com>
2025-07-24 15:53:57 +0100

    completed Task Counter

--- src/main/java/com/corndel/trees/task_counter/TaskCounter.java
+++ src/main/java/com/corndel/trees/task_counter/TaskCounter.java
@@ -1,15 +1,34 @@
 package com.corndel.trees.task_counter;
 
 import com.corndel.trees.dto.Task;
+import java.util.Stack;
 
 public class TaskCounter {
 
     public static int countIterative(Task task) {
-        return 0;
+        int count = 0;
+        Stack<Task> stack = new Stack<>();
+        stack.push(task);
+
+        while (!stack.isEmpty()) {
+            Task current = stack.pop();
+            count++;
+            for (Task child : current.getChildren()) {
+                stack.push(child);
+            }
+        }
+
+        return count;
     }
 
 
     public static int countRecursive(Task task) {
-        return 0;
+        int count = 1;
+
+        for (Task child: task.getChildren()) {
+            count += countRecursive(child);
+        }
+
+        return count;
     }
 }
