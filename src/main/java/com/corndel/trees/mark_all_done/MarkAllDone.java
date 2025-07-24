9888b731b36f0b2221a506eb0aa0b4d3975a25f6
mik-rzo <michael.panong3@gmail.com>
2025-07-24 16:39:09 +0100

    completed Mark All Done

--- src/main/java/com/corndel/trees/mark_all_done/MarkAllDone.java
+++ src/main/java/com/corndel/trees/mark_all_done/MarkAllDone.java
@@ -1,11 +1,27 @@
 package com.corndel.trees.mark_all_done;
 
 import com.corndel.trees.dto.Task;
+import java.util.Stack;
 
 public class MarkAllDone {
     public static void iterative(Task task) {
+        Stack<Task> stack = new Stack<>();
+        stack.push(task);
+
+        while (!stack.isEmpty()) {
+            Task current = stack.pop();
+            current.setDone(true);
+            for (Task child : current.getChildren()) {
+                stack.push(child);
+            }
+        }
     }
 
     public static void recursive(Task task) {
+        task.setDone(true);
+
+        for (Task child : task.getChildren()) {
+            recursive(child);
+        }
     }
 }
