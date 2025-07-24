8777779d8821b9dec58c555da2be860f46074146
mik-rzo <michael.panong3@gmail.com>
2025-07-24 16:56:18 +0100

    completed Done Counter

--- src/main/java/com/corndel/trees/done_counter/DoneCounter.java
+++ src/main/java/com/corndel/trees/done_counter/DoneCounter.java
@@ -1,15 +1,36 @@
 package com.corndel.trees.done_counter;
 
 import com.corndel.trees.dto.Task;
+import java.util.Stack;
 
 public class DoneCounter {
 
     public static int countIterative(Task task) {
-        return 0;
+        int count = 0;
+        Stack<Task> stack = new Stack<>();
+        stack.push(task);
+
+        while (!stack.isEmpty()) {
+            Task current = stack.pop();
+            if (current.isDone()) {
+                count++;
+            }
+            for (Task child : current.getChildren()) {
+                stack.push(child);
+            }
+        }
+
+        return count;
     }
 
     public static int countRecursive(Task task) {
-        return 0;
+        int currentCount = task.isDone() ? 1 : 0;
+
+        for (Task child : task.getChildren()) {
+            currentCount += countRecursive(child);
+        }
+
+        return currentCount;
     }
 
 }
