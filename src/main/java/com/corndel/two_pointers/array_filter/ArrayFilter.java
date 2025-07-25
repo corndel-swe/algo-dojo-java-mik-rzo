4c24b69db48784d680e96bc4b5fe572a5650305c
mik-rzo <michael.panong3@gmail.com>
2025-07-25 11:59:26 +0100

    completed Array Filter

--- src/main/java/com/corndel/two_pointers/array_filter/ArrayFilter.java
+++ src/main/java/com/corndel/two_pointers/array_filter/ArrayFilter.java
@@ -1,12 +1,44 @@
 package com.corndel.two_pointers.array_filter;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class ArrayFilter {
 
     public static int[] getDistinctNumbersBruteForce(int[] arr) {
-        return new int[0];
+        if (arr.length < 1) {
+            return new int[]{};
+        }
+
+        List<Integer> distinctNumbers = new ArrayList<>();
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (arr[i] != arr[i + 1]) {
+                distinctNumbers.add(arr[i]);
+            }
+        }
+
+        distinctNumbers.add(arr[arr.length - 1]);
+
+        return distinctNumbers.stream().mapToInt(Integer::intValue).toArray();
     }
 
     public static int[] getDistinctNumbersTwoPointers(int[] arr) {
-        return new int[0];
+        if (arr.length < 1) {
+            return new int[]{};
+        }
+
+        int[] copy = Arrays.copyOf(arr, arr.length);
+
+        int write = 1;
+        for (int read = 1; read < arr.length; read++) {
+            if (arr[read] != arr[read - 1]) {
+                copy[write] = arr[read];
+                write++;
+            }
+        }
+
+        return Arrays.copyOf(copy, write);
     }
 }
