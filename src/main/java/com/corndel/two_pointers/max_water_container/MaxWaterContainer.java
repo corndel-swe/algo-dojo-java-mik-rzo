84adcffaf76f747e5bf5e0418bff309bdb9a429c
mik-rzo <michael.panong3@gmail.com>
2025-07-26 11:48:30 +0100

    completed Max Water Container

--- src/main/java/com/corndel/two_pointers/max_water_container/MaxWaterContainer.java
+++ src/main/java/com/corndel/two_pointers/max_water_container/MaxWaterContainer.java
@@ -3,12 +3,44 @@ package com.corndel.two_pointers.max_water_container;
 public class MaxWaterContainer {
 
     public static int mostWaterBruteForce(int[] heights) {
-        return 0;
+        int maxArea = 0;
+
+        for (int i = 0; i < heights.length - 1; i++) {
+            for (int j = i + 1; j < heights.length; j++) {
+                int width = j - i;
+                int currentHeight = Math.min(heights[i], heights[j]);
+                int currentArea = width * currentHeight;
+                if (currentArea > maxArea){
+                    maxArea = currentArea;
+                }
+            }
+        }
+
+        return maxArea;
     }
 
 
     public static int mostWaterTwoPointers(int[] heights) {
-        return 0;
+        int left = 0;
+        int right = heights.length - 1;
+        int maxArea = 0;
+
+        while (left < right) {
+            int width = right - left;
+            int minHeight = Math.min(heights[left], heights[right]);
+            int currentArea = width * minHeight;
+            if (currentArea > maxArea){
+                maxArea = currentArea;
+            }
+
+            if (heights[left] < heights[right]){
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return maxArea;
     }
 
 }
