6332b4bb63a0b92974f28ea06ec0013b8ab97cc1
mik-rzo <michael.panong3@gmail.com>
2025-07-26 11:55:14 +0100

    completed Pair Sum

--- src/main/java/com/corndel/two_pointers/pair_sum/PairSum.java
+++ src/main/java/com/corndel/two_pointers/pair_sum/PairSum.java
@@ -2,11 +2,34 @@ package com.corndel.two_pointers.pair_sum;
 
 public class PairSum {
     public static boolean hasPairSumBruteForce(int[] arr, int target) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] + arr[j] == target) {
+                    return true;
+                }
+            }
+        }
+
         return false;
     }
 
 
     public static boolean hasPairSumTwoPointers(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left < right) {
+            int currentSum = arr[left] + arr[right];
+
+            if (currentSum == target) {
+                return true;
+            } else if (currentSum < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
         return false;
     }
 }
